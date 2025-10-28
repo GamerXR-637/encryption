0c804ce80be1a8b1db358da6fc2f74359d9b04ae
Gamer-XR637 <168220319+GamerXR-637@users.noreply.github.com>
2025-10-28 16:56:52 -0400

    Create index.js

---
+++ BetterTools/index.js
@@ -0,0 +1,9 @@
+const { Randcolor } = require("./color.js");
+const { betterUUID } = require("./betterUUID.js");
+const { FtM } = require("./FtM.js");
+
+module.exports = {
+  Randcolor,
+  betterUUID,
+  FtM,
+};

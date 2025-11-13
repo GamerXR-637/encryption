b9397b2db86108f36e47f2e8bd4e856308c6dae0
Gamer-XR637 <168220319+GamerXR-637@users.noreply.github.com>
2025-11-13 09:07:00 -0500

    Update index.js

--- BetterTools/index.js
+++ BetterTools/index.js
@@ -1,55 +1,55 @@
-const { betterUUID } = require("./tools/betterUUID.js");
 const { colorTerminal } = require("./tools/colorTerminal.js");
-const { FtM } = require("./useless/FtM.js");
+const { seededRandom } = require("./tools/seededRandom.js");
+const { betterUUID } = require("./tools/betterUUID.js");
 const { Randcolor } = require("./tools/Randcolor.js");
 const { rgbToHex } = require("./tools/rgbtohex.js");
 const { sleep } = require("./tools/sleep.js");
-const { seededRandom } = require("./tools/seededRandom.js");
 
-const { average } = require("./math/average.js");
-const { circleArea } = require("./math/circleArea.js");
 const { cylinderVolume } = require("./math/cylinderVolume.js");
-const { inRange } = require("./math/inRange.js");
-const { median } = require("./math/median.js");
+const { multiplyPoly } = require("./math/multiplyPoly.js");
+const { pointSlope } = require("./math/pointSlope.js");
+const { circleArea } = require("./math/circleArea.js");
 const { randomInt } = require("./math/randomInt.js");
+const { quadratic } = require("./math/quadratic.js");
 const { triangle } = require("./math/triangle.js");
+const { average } = require("./math/average.js");
+const { inRange } = require("./math/inRange.js");
+const { percent } = require("./math/percent.js");
+const { median } = require("./math/median.js");
 const { pointY } = require("./math/pointY.js");
 const { pointX } = require("./math/pointX.js");
-const { multiplyPoly } = require("./math/multiplyPoly.js");
-const { percent } = require("./math/percent.js");
-const { pointSlope } = require("./math/pointSlope.js");
-const { quadratic } = require("./math/quadratic.js");
 
-const { truncate } = require("./text/truncate.js");
 const { textToBinary } = require("./text/binary.js");
 const { morseCode } = require("./text/morseCode.js");
+const { truncate } = require("./text/truncate.js");
 
+const { FtM } = require("./useless/FtM.js");
 const { cat } = require("./useless/cat.js");
 
 module.exports = {
-  average,
-  betterUUID,
-  circleArea,
   cylinderVolume,
   colorTerminal,
-  FtM,
-  inRange,
-  Randcolor,
-  randomInt,
-  rgbToHex,
-  sleep,
-  triangle,
-  truncate,
-  median,
-  textToBinary,
-  morseCode,
-  cat,
-  pointX,
-  pointY,
   multiplyPoly,
   seededRandom,
-  percent,
+  textToBinary,
   pointSlope,
+  betterUUID,
+  circleArea,
   quadratic,
+  Randcolor,
+  morseCode,
+  randomInt,
+  triangle,
+  truncate,
+  rgbToHex,
+  average,
+  inRange,
+  percent,
+  pointY,
+  pointX,
+  median,
+  sleep,
+  FtM,
+  cat,
 };
 
